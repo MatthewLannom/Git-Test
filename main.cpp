e1356bfd92695effa5df025b561aa8e950507768
Matthew Lannom <matdlann@ut.utm.edu>
2023-09-08 01:12:32 -0500

    Changed this file so it now finds a sum

--- main.cpp
+++ main.cpp
@@ -3,8 +3,22 @@
 
 using namespace std;
 
+int sum (int n);
+
 int main() {
-  cout << "Hello world!" << endl;
+  int n;
+  cout << "Please enter a number." << endl;
+  cin >> n;
+  cout << sum(n) << endl;
   
   return 0;
+}
+
+int sum (int n){
+  int count = 1; int sum = 0;
+  while (count <= n){
+    sum += count;
+    count ++;
+  }
+  return sum;
 }
